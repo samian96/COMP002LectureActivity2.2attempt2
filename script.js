425c240dd351549248f049e857b8837c86b73728
samian96 <sam.iannazzo@yahoo.com>
2025-10-23 00:19:05 -0700

    added variable for myGreeting / const for pi / let myNumber

---
+++ script.js
@@ -0,0 +1,14 @@
+let myGreeting = "Hello, World";
+console.log("Hello, World!")
+/////////////////////////////////
+const pi = 3.14;
+console.log(pi); // math.min does not do anything to pi when running debug
+
+/*let pi = 2;
+* console.log(pi);
+* does not work due to pi is assigned to a constant, that is always running
+*/
+/////////////////////////////////
+let myNumber = 4;
+console.log(Math.min(2,10,4,18,7)) //math.min shows the smallest of zero or more numbers provided in the console.log
+
